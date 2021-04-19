00c7f0af52e177bbcecd08608cf2dbc9d3134d30
Ter_4333 <yarik.krapka125@gmail.com>
2021-04-19 14:00:51 +0300

    HW on 24|04|21

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
-data = int(input("Напишы любое число: "))
-answer = 1
-while (data / 2) % 2 == 0:
-    answer += 1
-    data = data / 2
+data = str(input("String: "))
+answer = ""
+for y in data:
+    if y.isdigit():
+        answer += y
 print(answer)
