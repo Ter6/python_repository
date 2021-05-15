f59b45e7bf4f0c5fe58f0b5f6f81991293c0d582
Ter_4333 <yarik.krapka125@gmail.com>
2021-05-15 19:38:13 +0300

    Game 2048 v-0,01_alfa

--- task2.py
+++ task2.py
@@ -1,48 +1,44 @@
-
-
 def print_field2048(field2048):
     string = ""
-    for y in range(len(field2048)):
+    for line in range(len(field2048)):
         string += " _"
     print(string)
     string = ""
-    for y in field2048:
-        for u in y:
+    for line in field2048:
+        for u in line:
             string += f"{u}|"
         print(f"|{string}")
         string = ""
-    for y in range(len(field2048)):
+    for line in range(len(field2048)):
         string += " ¯"
     print(string)
 
 
 def move_right_field2048(field2048):
-    for y in field2048:
-        for u in range(len(y)):
-            for t in range(len(y)):
-                if y[u] == 0:
-                    y[u] = y[t]
-                    y[t] = 0
-                if y[u] == y[t] and t != u:
-                    y[t] += y[u]
-                    y[u] = 0
+    for line in field2048:
+        for symbol in range(len(line) - 1):
+            if line[symbol + 1] == 0:
+                line[symbol] = line[symbol + 1]
+                line[symbol + 1] = 0
+            if line[symbol] == line[symbol + 1] and symbol + 1 != symbol:
+                line[symbol + 1] += line[symbol]
+                line[symbol] = 0
     return field2048
 
 
 def move_left_field2048(field2048):
-    for r in range(len(field2048[0])):
-        for y in field2048:
-            for u in range(len(y) - 1, -1, -1):
-                for t in range(len(y) - 1, -1, -1):
-                    print(y[u], y[t], field2048)
-                    if y[u] == 0:
-                        y[u] = y[t]
-                        y[t] = 0
-                    if y[u] == y[t] and t != u:
-                        y[t] += y[u]
-                        y[u] = 0
+    for amount in range(len(field2048)):
+        for line in field2048:
+            for symbol in range(len(line) - 2, -2, -1):
+                print(line[symbol], line[symbol + 1], line)
+                if line[symbol + 1] == 0:
+                    line[symbol + 1] = line[symbol]
+                    line[symbol + 1] = 0
+                if line[symbol] == line[symbol + 1] and symbol + 1 != symbol:
+                    line[symbol + 1] += line[symbol]
+                    line[symbol] = 0
     return field2048
 
 
-field = [[0, 0, 2, 2], [0, 0, 2, 2], [0, 0, 2, 4], [8, 8, 4, 2]]
+field = [[2, 2, 4, 4], [0, 0, 0, 0], [6, 6, 12, 0], [8, 8, 8, 8]]
 print_field2048(move_left_field2048(field))
